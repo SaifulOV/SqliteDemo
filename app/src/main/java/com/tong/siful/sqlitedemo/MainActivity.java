cedf30ffab6d055af907ae3179070369e89887cb
Saiful Islam Ovi <ovi@softbyteit.com>
2018-09-20 18:50:22 +0600

    All done (CRUD) .

--- app/src/main/java/com/tong/siful/sqlitedemo/MainActivity.java
+++ app/src/main/java/com/tong/siful/sqlitedemo/MainActivity.java
@@ -1,45 +1,65 @@
 package com.tong.siful.sqlitedemo;
+
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     MyDatabaseHelper myDatabaseHelper;
-    private EditText nameEditText, ageEditText, genderEditText;
-    private Button saveButton, showButton;
+    private EditText nameEditText, ageEditText, genderEditText, idEditText;
+    private Button saveButton, showButton, updateButton, deleteButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         myDatabaseHelper = new MyDatabaseHelper(MainActivity.this);
         SQLiteDatabase sqLiteDatabase = myDatabaseHelper.getWritableDatabase();
+
         nameEditText = findViewById(R.id.nameEdId);
         ageEditText = findViewById(R.id.ageEdId);
         genderEditText = findViewById(R.id.genderEdId);
+        idEditText = findViewById(R.id.idEdId);
+
         saveButton = findViewById(R.id.saveBtnId);
         showButton = findViewById(R.id.showBtnId);
+        updateButton = findViewById(R.id.updateBtnId);
+        deleteButton = findViewById(R.id.deleteBtnId);
+
+        updateButton.setOnClickListener(this);
         saveButton.setOnClickListener(this);
         showButton.setOnClickListener(this);
+        deleteButton.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View view) {
         String name = nameEditText.getText().toString();
         String age = ageEditText.getText().toString();
         String gender = genderEditText.getText().toString();
-        System.out.println("--------------------------------");
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(gender);
+        String id = idEditText.getText().toString();
+
+
+        Log.v("info", "Name is : " + name);
+        Log.v("info", "Age is : " + age);
+        Log.v("info", "Gender is : " + gender);
+        Log.v("info", "Id is : " + id);
+
+        //save Data
         if (view.getId() == R.id.saveBtnId) {
             if (name.length() == 0 && age.length() == 0 && gender.length() == 0) {
                 Toast.makeText(MainActivity.this, "FIELD CANNOT BE EMPTY", Toast.LENGTH_LONG).show();
             } else {
                 long rowId = myDatabaseHelper.insertData(name, age, gender);
+
                 if (rowId == -1) {
                     Toast.makeText(getApplicationContext(), "unsuccessful", Toast.LENGTH_LONG).show();
                 } else {
@@ -50,24 +70,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         }
+        //show data
         if (view.getId() == R.id.showBtnId) {
             SQLiteDatabase sqLiteDatabase = myDatabaseHelper.getWritableDatabase();
             Cursor resultSet = myDatabaseHelper.displayData();
+
             if (resultSet.getCount() == 0) {
+
                 //if there is no data
                 showData("Error", "No Data Found");
                 return;
             }
+
             StringBuffer stringBuffer = new StringBuffer();
+
             while (resultSet.moveToNext()) {
                 stringBuffer.append("Id     : " + resultSet.getString(0) + "\n");
                 stringBuffer.append("Name   : " + resultSet.getString(1) + "\n");
                 stringBuffer.append("Age    : " + resultSet.getString(2) + "\n");
                 stringBuffer.append("Gender : " + resultSet.getString(3) + "\n\n\n");
             }
+
             showData("ResultSet", stringBuffer.toString());
         }
+
+        //update Data
+        else if (view.getId() == R.id.updateBtnId) {
+
+            Boolean isUpdated = myDatabaseHelper.updateData(id, name, age, gender);
+
+            if (isUpdated == true) {
+                Toast.makeText(getApplicationContext(), "Data Is successfully updated", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Data Is not updated", Toast.LENGTH_LONG).show();
+            }
+
+        } else if (view.getId() == R.id.deleteBtnId) {
+            int value = myDatabaseHelper.deleteData(id);
+
+            if (value > 0) {
+                Toast.makeText(getApplicationContext(), "Data Is delete", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Data Is not delete", Toast.LENGTH_LONG).show();
+            }
+        }
     }
+
+
+    //display data by alert dialog
     public void showData(String title, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(title);
--- app/src/main/java/com/tong/siful/sqlitedemo/MyDatabaseHelper.java
+++ app/src/main/java/com/tong/siful/sqlitedemo/MyDatabaseHelper.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
+
 public class MyDatabaseHelper extends SQLiteOpenHelper {
     private Context context;
     private static final String DATABASE_NAME = "student.db";
@@ -20,30 +21,35 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME + " VARCHAR(255) ," + AGE + " INTEGER , " + GENDER + " VARCHAR(255) ); ";
     private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
     private static final String SHOW_DATA = "SELECT * FROM " + TABLE_NAME;
+
     //constructor
     public MyDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION_NUMBER);
         this.context = context;
     }
+
+    //create table
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         try {
-            Toast.makeText(context, "onCreate is Called ", Toast.LENGTH_LONG).show();
             sqLiteDatabase.execSQL(CREATE_TABLE);
         } catch (Exception e) {
-            Toast.makeText(context, "Exception : " + e, Toast.LENGTH_LONG).show();
+            e.printStackTrace();
         }
     }
+
+    //    update Table
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         try {
-            Toast.makeText(context, "onUpgrade is Called ", Toast.LENGTH_LONG).show();
             sqLiteDatabase.execSQL(DROP_TABLE);
             onCreate(sqLiteDatabase);
         } catch (Exception e) {
-            Toast.makeText(context, "Exception : " + e, Toast.LENGTH_LONG).show();
+            e.printStackTrace();
         }
     }
+
+    //insert data
     public long insertData(String name, String age, String gender) {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -53,9 +59,32 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         long rowId = sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
         return rowId;
     }
-    public Cursor displayData(){
+
+    //display data
+    public Cursor displayData() {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery(SHOW_DATA,null);
+        Cursor cursor = sqLiteDatabase.rawQuery(SHOW_DATA, null);
         return cursor;
+    }
+
+    //update data
+    public boolean updateData(String id, String name, String age, String gender) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ID, id);
+        contentValues.put(NAME, name);
+        contentValues.put(AGE, age);
+        contentValues.put(GENDER, gender);
+
+        sqLiteDatabase.update(TABLE_NAME, contentValues, ID + " = ? ", new String[]{id});
+        return true;
+
+    }
+
+    //Delete Data
+    public int deleteData(String id) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        return sqLiteDatabase.delete(TABLE_NAME, ID + " = ? ", new String[]{id});
+
     }
 }
